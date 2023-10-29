bc41b7d0be0b90a902669b9b1ea1b9d5c7eb8fee
SrWither <fjachuf@gmail.com>
2023-10-29 20:18:01 -0300

    test

--- lua/utils/colors.lua
+++ lua/utils/colors.lua
@@ -1,32 +1,24 @@
 local M = {}
 
-function M.get_installed_colorschemes()
-    local installed_colorschemes = {}
+function M.get_installed_colorschemes(opts)
+  local before_background = vim.o.background
+  local before_color = vim.api.nvim_exec("colorscheme", true)
+  local need_restore = true
 
-    -- Get the current background and color scheme
-    local before_background = vim.o.background
-    local before_color = vim.api.nvim_exec("colorscheme", true)
+  local colors = opts.colors or { before_color }
+  if not vim.tbl_contains(colors, before_color) then
+    table.insert(colors, 1, before_color)
+  end
 
-    -- Get a list of available colorschemes
-    local available_colors = vim.fn.getcompletion("", "color")
+  colors = vim.list_extend(
+    colors,
+    vim.tbl_filter(function(color)
+      return color ~= before_color
+    end, vim.fn.getcompletion("", "color"))
+  )
 
-    -- Ensure the current colorscheme is included in the list
-    if not vim.tbl_contains(available_colors, before_color) then
-        table.insert(available_colors, 1, before_color)
-    end
+  return colors
 
-    -- Filter out duplicates and add them to the installed_colorschemes table
-    for _, color in ipairs(available_colors) do
-        if not vim.tbl_contains(installed_colorschemes, color) then
-            table.insert(installed_colorschemes, color)
-        end
-    end
-
-    -- Restore the original background and colorscheme
-    vim.o.background = before_background
-    vim.cmd("colorscheme " .. before_color)
-
-    return installed_colorschemes
 end
 
 return M
